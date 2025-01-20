435e49d0d08f3ef52de03b52c2deb65bc0169ec0
arpithaa-c <arpithanaik66@gmail.com>
2025-01-21 01:37:04 +0530

    Initiaal commit

---
+++ VariablesDataTypes/Overview.py
@@ -0,0 +1,3 @@
+#1 Python is Dynamically Typed Programming language.
+#In python while Declaring Variables we no need to mention Datatypes.
+#DataTypes will be assigned by language based on values stored into the variables.
